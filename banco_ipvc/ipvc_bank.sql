38fa226b44288c884bcdd29e330b222ea38896df
Lucas Maciel <03lucasmaciel@gmail.com>
2024-03-01 13:54:48 +0000

    feat add exercises 1.2.1, 1.2.2, 1.2.3, 1.2.4 ✅

--- banco_ipvc/ipvc_bank.sql
+++ banco_ipvc/ipvc_bank.sql
@@ -1,3 +1,4 @@
+-- Active: 1709286079626@@localhost@1433@bancoei27712
 -- SQL SERVER - TSQL - VIEWS, SP's, Triggers e Cursores
 -- 1 
 -- Uma conta pode ter vários Clientes e um Cliente pode ter várias contas. Nas contas bancárias existem dois tipos de movimentos:
@@ -36,3 +37,68 @@ CREATE TABLE Movimento (
     FOREIGN KEY (IdConta) REFERENCES Conta(IdConta)
 );
 
+--? Alterar campo TipoMovimento, pois só pode ser "C" ou "D"
+ALTER TABLE Movimento ADD CONSTRAINT CHK_TipoMovimento CHECK (TipoMovimento IN ('C', 'D'));
+
+-- 1.2 Instruções SQL
+--  1.2.1 Instrução
+--      Inserir um campo na tabela conta, chamado tipo_conta para classificar se é uma conta de R de "risco",E de "estável", e P de "potencial investidor"e por defeito "Sem movimentos"
+ALTER TABLE Conta ADD Tipo_conta CHAR(1) DEFAULT 'S' CHECK (tipo_conta IN ('R','E','P','S'))
+
+-- 1.2.2 Instrução
+--      Inserir um campo na tabela cliente, chamado tipo para classificar se o cliente é I de "individual"ou E de "empresa"
+ALTER TABLE Cliente ADD Tipo CHAR(1) DEFAULT 'I' CHECK (tipo IN ('I','E'))
+
+-- 1.2.3 Instrução
+--      Inserir um campo na tabela conta, chamado definicao_conta para definir conta a P de prazo, N de normal e C de certificados do tesouro.
+ALTER TABLE Conta ADD Definicao_conta CHAR(1) DEFAULT 'N' CHECK (definicao_conta IN ('P','N','C'))
+
+-- 1.2.3.4 Carregamento de Dados
+--      Aceder ao mockaroo (gerador de dados aleatórios) e gerar scripts sql para car- regar com dados as tabelas criadas tendo em conta as restrições de integridade de domínio.
+--      Inserir 10 Clientes
+insert into Cliente (Nome, Tipo) values ('Ody', 'E');
+insert into Cliente (Nome, Tipo) values ('Claudio', 'E');
+insert into Cliente (Nome, Tipo) values ('Rafaela', 'E');
+insert into Cliente (Nome, Tipo) values ('Stepha', 'I');
+insert into Cliente (Nome, Tipo) values ('Sadella', 'E');
+insert into Cliente (Nome, Tipo) values ('Katalin', 'E');
+insert into Cliente (Nome, Tipo) values ('Devi', 'I');
+insert into Cliente (Nome, Tipo) values ('Valentina', 'E');
+insert into Cliente (Nome, Tipo) values ('Wilhelmina', 'E');
+insert into Cliente (Nome, Tipo) values ('Shawnee', 'I');
+
+--      Inserir 10 Contas
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (7593.5, 'S', 'N');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (6405.08, 'P', 'C');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (3140.72, 'P', 'N');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (6876.29, 'E', 'N');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (4993.73, 'E', 'C');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (8720.49, 'E', 'C');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (4504.55, 'P', 'P');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (2395.58, 'P', 'N');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (3309.49, 'S', 'P');
+insert into Conta (Saldo, Tipo_conta, Definicao_conta) values (974.48, 'E', 'N');
+
+--      Inserir 10 Titulares
+insert into Titular_Conta (IdConta, IdCliente) values (3, 1);
+insert into Titular_Conta (IdConta, IdCliente) values (1, 2);
+insert into Titular_Conta (IdConta, IdCliente) values (5,4);
+insert into Titular_Conta (IdConta, IdCliente) values (9, 5);
+insert into Titular_Conta (IdConta, IdCliente) values (2,3);
+insert into Titular_Conta (IdConta, IdCliente) values (4, 6);
+insert into Titular_Conta (IdConta, IdCliente) values (7, 7);
+insert into Titular_Conta (IdConta, IdCliente) values (8,9);
+insert into Titular_Conta (IdConta, IdCliente) values (10, 8);
+insert into Titular_Conta (IdConta, IdCliente) values (6, 10);
+
+--      Inserir 10 Movimentos
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (9, '2023-12-29', 'D', 7905.39);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (6, '2023-05-24', 'C', 9479.5);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (8, '2023-07-19', 'C', 986.67);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (7, '2023-05-29', 'D', 7280.15);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (4, '2023-05-14', 'D', 9338.85);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (3, '2023-12-14', 'D', 8269.18);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (5, '2023-06-20', 'C', 5523.38);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (2, '2024-01-24', 'C', 783.98);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (1, '2023-08-10', 'C', 7821.51);
+insert into Movimento (IdConta, Data, TipoMovimento, Valor) values (10, '2023-05-25', 'D', 2984.78);
